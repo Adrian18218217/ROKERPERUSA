7d07751e561782aef81914194f5ca0d5b29cd663
Adrian <U18218217@utp.edu.pe>
2020-11-27 13:15:21 -0500

    Proyecto inicializado sin configuraciones

---
+++ src/test/java/com/rokerperusa/ProyectRokerPeruSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rokerperusa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectRokerPeruSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
